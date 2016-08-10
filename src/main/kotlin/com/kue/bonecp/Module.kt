b375d2317c2db60e33e63ab68d0d636388ac70b7
Michael Vaughan <mike.vaughan@qonceptual.com>
2016-08-09 23:25:49 -0400

    1.0.0

--- src/main/kotlin/com/kue/bonecp/Module.kt
+++ src/main/kotlin/com/kue/bonecp/Module.kt
@@ -1,14 +1,20 @@
 package com.kue.bonecp
 
 import com.google.inject.AbstractModule
-import javax.sql.DataSource
+
+import com.kue.core.NamedDataSourceCollection
+import javax.inject.Inject
 
 /**
  * @author Michael Vaughan
  */
-class Module : AbstractModule() {
+class Module @Inject constructor() : AbstractModule() {
 
     override fun configure() {
-        bind(DataSource::class.java).toProvider(BoneCPDataSourceProvider::class.java).asEagerSingleton()
+        bind(NamedDataSourceCollection::class.java)
+                .toProvider(BoneCPDataSourceProvider::class.java)
     }
+
+
+
 }
